b7ab25bddc31ca86a1af260390a9c8159f4aa445
Eugene Leontev <potehin@gmail.com>
2026-04-10 22:45:09 +0400

    fix i18n format - simple strings instead of ru/en

--- src/i18n/messages/rapidTechniquesFlow.ts
+++ src/i18n/messages/rapidTechniquesFlow.ts
@@ -1,12 +1,12 @@
 import { i18n } from '../setup'
 
 export const rapidTechniquesFlowMessages = i18n('rapidTechniquesFlow', {
-  homeCtaTitle: { ru: 'Дыхание 4-6', en: 'Breathing 4-6' },
-  homeCtaDescription: { ru: 'Быстрое успокоение за 1 минуту', en: 'Quick calming in 1 minute' },
-  back: { ru: 'Назад', en: 'Back' },
-  finish: { ru: 'Готово', en: 'Done' },
+  homeCtaTitle: 'Дыхание 4-6',
+  homeCtaDescription: 'Быстрое успокоение за 1 минуту',
+  back: 'Назад',
+  finish: 'Готово',
   steps: {
-    breathing46Title: { ru: 'Дыхание 4–6', en: 'Breathing 4-6' },
-    breathing46Subtitle: { ru: 'Вдох 4 секунды, выдох 6 секунд. Повторить 3 раза.', en: 'Inhale 4 seconds, exhale 6 seconds. Repeat 3 times.' },
+    breathing46Title: 'Дыхание 4–6',
+    breathing46Subtitle: 'Вдох 4 секунды, выдох 6 секунд. Повторить 3 раза.',
   },
 } as any)
